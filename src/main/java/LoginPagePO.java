1daa1601ccb1743ac846d15b1bc82244aa5b86d3
Tony Chung <tony@hi.com>
2022-12-02 09:55:42 +0800

    Initial commit

---
+++ src/main/java/LoginPagePO.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.WebDriver;
+
+
+public class LoginPagePO {
+
+    WebDriver webDriver;
+    private final By phoneNumberTextBox = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText");
+    private final By helpButton = By.xpath("//android.widget.Button[@content-desc=\"幫助\"]");
+    private final By confirmButton = By.xpath("//android.widget.Button[@content-desc=\"確認\"]");
+
+
+    public void clickHelpButton()
+    {
+        webDriver.findElement(helpButton).click();
+    }
+
+    public void clickConfirmButton()
+    {
+        webDriver.findElement(confirmButton).click();
+    }
+    public void inputPhoneNumber(String phoneNum)
+    {
+        webDriver.findElement(phoneNumberTextBox).sendKeys(phoneNum);
+    }
+
+    public void loginDriverApp(String strPhoneNum)
+    {
+        //inputPhoneNumber(strPhoneNum);
+        clickConfirmButton();
+    }
+}
---
+++ src/test/java/TestWorkshop.java
@@ -0,0 +1,63 @@
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+
+
+public class TestWorkshop {
+    WebDriver webDriver;
+    WebDriverWait webDriverWait;
+    LoginPagePO loginPagePO = new LoginPagePO();
+    @Before
+    public void beforeTest() throws MalformedURLException {
+        // Set common desired capabilities
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10");
+        capabilities.setCapability(MobileCapabilityType.UDID, "emulator-5554");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2"); //android automation
+        capabilities.setCapability(MobileCapabilityType.FULL_RESET, "false");
+        capabilities.setCapability(MobileCapabilityType.NO_RESET, "false");
+        capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, "600000"); //milliseconds
+        capabilities.setCapability(MobileCapabilityType.APP, "/Users/tony/Desktop/app-dev-debug-1127.apk");
+
+        // Set android desired capabilities
+        capabilities.setCapability(AndroidMobileCapabilityType.SYSTEM_PORT, "1253"); //must not in use
+
+        // Launch Mobile App
+
+        webDriver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), capabilities);
+
+        // Create Webdriver Wait Object
+        webDriverWait = new WebDriverWait(webDriver, Duration.ofSeconds(30));
+    }
+
+    @After
+    public void afterTest(){
+        // Close Chrome Browser
+        webDriver.quit();
+    }
+
+    @Test
+    public void Test1() throws InterruptedException {
+
+        loginPagePO.clickHelpButton();
+        Thread.sleep(5000);
+
+    }
+
+}
